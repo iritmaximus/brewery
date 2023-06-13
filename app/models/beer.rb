fdb6de1f4a0ee48508879bb9bd33b7d6735061aa
iritmaximus <aaro.koivukangas@gmail.com>
2023-06-13 13:08:42 +0300

    week 2 up to 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,12 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    average = 0
+    self.ratings.each do |rating|
+      average += rating.score
+    end
+    return average
+  end
 end
