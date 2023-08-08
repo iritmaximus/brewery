23e6537a122d91358ae1d0314245fb74b3daedc1
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2023-08-08 20:01:01 +0300

    upto 3.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
   resources :breweries
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -6,5 +7,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/', to: 'breweries#index'
+  get 'signup', to: 'users#new'
   resources :ratings, only: [:index, :new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 end
