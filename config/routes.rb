403a3c36744a65b3b5a1de5cc4603dbab78b8150
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2023-08-08 10:32:52 +0300

    upto 2.11

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/', to: 'breweries#index'
-  get 'ratings', to: 'ratings#index'
-  post 'ratings', to: 'ratings#create'
-  get 'ratings/new', to: 'ratings#new'
+  resources :ratings, only: [:index, :new, :create, :destroy]
 end
