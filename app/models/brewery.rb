80e1c6407dd7156685ad9a057f4e634b3625429e
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2023-08-08 11:08:38 +0300

    upto 2.14, added rating average counting for breweries

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,6 @@
 class Brewery < ApplicationRecord
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
 
   def print_report
     puts name
@@ -10,5 +11,31 @@ class Brewery < ApplicationRecord
   def restart
     self.year = 2023
     puts "changed year to #{2023}"
+  end
+
+  def average_rating
+    total_score = 0
+    count = self.count_ratings
+    if count == 0
+      return 0
+    end
+
+    self.beers.each do |beer|
+      beer.ratings.each do |rating|
+        total_score += rating.score
+      end
+    end
+
+     return total_score / count
+    "This brewery has #{count} #{pluralize count, "rating"} with an average of #{average}"
+  end
+
+  def count_ratings
+    count = 0
+    self.beers.each do |beer|
+      count += beer.ratings.count
+    end
+
+    return count
   end
 end
