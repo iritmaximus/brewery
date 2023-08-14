86e46cb954a49a50da79481031f396b1612a6813
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2023-08-14 17:57:21 +0300

    upto 3.16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,13 +39,9 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-    if not @user == current_user
-      redirect_to user_path(@user), notice: "User not authorized to edit this user"
-      return
-    end
 
     respond_to do |format|
-      if @user.update(user_params)
+      if user_params[:username].nil? && @user == current_user && @user.update(user_params)
         format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
       else
