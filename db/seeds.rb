2ddc155dec6b39d8d17a9843225bf143d1b93d71
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2024-03-05 10:02:56 +0200

    changed selfhost to use better dockerfile + bugfixed entrypoint + seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,15 @@ b1 = Brewery.create name: "Koff", year: 1897
 b2 = Brewery.create name: "Malmgard", year: 2001
 b3 = Brewery.create name: "Weihenstephaner", year: 1040
 
-b1.beers.create name: "Iso 3", style: "Lager"
-b1.beers.create name: "Karhu", style: "Lager"
-b1.beers.create name: "Tuplahumala", style: "Lager"
-b2.beers.create name: "Huvila Pale Ale", style: "Pale Ale"
-b2.beers.create name: "X Porter", style: "Porter"
-b3.beers.create name: "Hefeweizen", style: "Weizen"
-b3.beers.create name: "Helles", style: "Lager"
+s1 = Style.create name: "Lager"
+s2 = Style.create name: "Pale Ale"
+s3 = Style.create name: "Portner"
+s4 = Style.create name: "Weizen"
+
+b1.beers.create name: "Iso 3", style: s1
+b1.beers.create name: "Karhu", style: s1
+b1.beers.create name: "Tuplahumala", style: s1
+b2.beers.create name: "Huvila Pale Ale", style: s2
+b2.beers.create name: "X Porter", style: s3
+b3.beers.create name: "Hefeweizen", style: s4
+b3.beers.create name: "Helles", style: s1
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-bundle exec rails assets:precompile
+bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
 
